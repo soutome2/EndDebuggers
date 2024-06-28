558c6fd273b1e3a7b83991e33227f199722f64b3
soutome <soutome.924@gmail.com>
2024-06-28 11:18:08 +0900

    1回目のコミット

---
+++ src/main/java/com/example/demo/EndlessDebuggersApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndlessDebuggersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndlessDebuggersApplication.class, args);
+	}
+
+}
