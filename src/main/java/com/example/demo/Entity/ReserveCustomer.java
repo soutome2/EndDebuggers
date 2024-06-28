033356c272702cc9a074e107bc674d42ccd842e1
soutome <soutome.924@gmail.com>
2024-06-28 16:37:13 +0900

    Entity合体させたやつ

---
+++ src/main/java/com/example/demo/Entity/ReserveCustomer.java
@@ -0,0 +1,44 @@
+package com.example.demo.Entity;
+
+import java.sql.Time;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+
+public class ReserveCustomer {
+	
+	@Id
+	@Column(name = "cid")
+	private String cid;
+	
+	@Column(name = "cname")
+	private String cname;
+	
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "tel")
+	private String tel;
+	
+	@Column(name = "mailaddress")
+	private String mailaddress;
+	
+	@Column(name = "reserveid")
+	private Integer reserveid;
+	
+	@Column(name = "reservedate")
+	private Date reservedate;
+	
+	@Column(name = "reservetime")
+	private Time reservetime;
+	
+	@Column(name = "detail")
+	private String detail;
+
+}
