f26a4f5828c3518acf6c5191e1f3b8ab82c6150f
Aki330 <ap_aki330@icloud.com>
2024-07-01 14:00:45 +0900

    OinputFormにgetEntityメゾットの追加　Reserve.javaに自動採番のメンバ変数追加reserve.htmlのnameを変更

--- src/main/java/com/example/demo/Entity/Reserve.java
+++ src/main/java/com/example/demo/Entity/Reserve.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data; 
@@ -16,6 +18,7 @@ import lombok.Data;
 public class Reserve {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name = "reserveid")
 	private Integer reserveid;
 	
--- src/main/java/com/example/demo/Form/OInputForm.java
+++ src/main/java/com/example/demo/Form/OInputForm.java
@@ -3,14 +3,25 @@ package com.example.demo.Form;
 import 	java.sql.Time;
 import java.util.Date;
 
+import com.example.demo.Entity.Reserve;
+
 import lombok.Data;
 @Data
 public class OInputForm {
 
-	 private Integer reserveid;
 	 private String  cid ;
-	 private String ename;
-	 private Date reservedate; 
-	 private Time reservetime;
+	 private String password; 
+	 private Date  reservedate; 
+	 private Time  reservetime;
 	 private String detail;
+	 
+	 public Reserve getEntity() {
+			Reserve reserve=new Reserve();
+			reserve.setCid(cid);
+			reserve.setReservedate(reservedate);
+			reserve.setReservetime(reservetime);
+			reserve.setDetail(detail);
+			
+			return reserve;
+	 }
 }
