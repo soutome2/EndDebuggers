b9a4317e0610d3a99ac3495bfe095496afca58da
skachi0117 <kasyunatts@gmail.com>
2024-07-01 13:32:15 +0900

    ログイン画面

--- src/main/java/com/example/demo/Controller/AceController.java
+++ src/main/java/com/example/demo/Controller/AceController.java
@@ -3,6 +3,7 @@ package com.example.demo.Controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,38 +22,42 @@ import lombok.AllArgsConstructor;
 public class AceController {
 	private final ReserveCustomerRepository reserveCustomerRepository;
 	private final HttpSession session;
-	
+
 	@GetMapping("/")
-	public String GetHome(){
+	public String GetHome() {
 		return "home";
-		
+
 	}
-	
+
 	@GetMapping("/Login")
-	public String GetUserLogin(){
+	public String GetUserLogin(LoginForm loginForm) {
 		return "login";
-		
+
 	}
-	
-	@PostMapping("/customer")
-	public ModelAndView showCostomerList(@ModelAttribute LoginForm loginForm,
-			ModelAndView mv) {
-		
-		String cid = loginForm.getCid();
-		List<ReserveCustomer> list = reserveCustomerRepository.findAIIBycustomerId(cid);
-		mv.addObject("reserveList", list);
-		mv.addObject("cname", "佐々木希");
-		mv.setViewName("customer");
-		return mv;
+
+	@PostMapping("/Customer")
+	public ModelAndView showCustomerList(@ModelAttribute LoginForm loginForm,
+			BindingResult result, ModelAndView mv) {
+
+		if (!result.hasErrors()) {
+			String cid = loginForm.getCid();
+			List<ReserveCustomer> list = reserveCustomerRepository.findAIIBycustomerId(cid);
+			mv.addObject("reserveList", list);
+			mv.addObject("cname", "佐々木希");
+			mv.setViewName("customer");
+			return mv;
+		} else {
+			mv.setViewName("userLogin");
+			return mv;
+		}
 	}
-	
+
 	@PostMapping("/Reserve")
-	public String PostReserve(@RequestParam("ename")  String ename){
-		session.setAttribute("ename",ename);
-		
+	public String PostReserve(@RequestParam("ename") String ename) {
+		session.setAttribute("ename", ename);
+
 		return "reserveInput";
-		
+
 	}
 
 }
-
--- src/main/java/com/example/demo/Form/LoginForm.java
+++ src/main/java/com/example/demo/Form/LoginForm.java
@@ -1,9 +1,14 @@
 package com.example.demo.Form;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginForm {
+	
+	@NotBlank(message = "利用者IDを入力して下さい")
 	private String cid;
+	
+	@NotBlank(message = "パスワードを入力して下さい")
 	private String password;
 }
