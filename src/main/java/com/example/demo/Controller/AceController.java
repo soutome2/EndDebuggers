d42782fe6473eb04f0aa8c3a5081b5936d33bc6e
Aki330 <ap_aki330@icloud.com>
2024-07-01 12:47:45 +0900

    homeクラスから予約登録ページに移行するときのControllerクラスの作成

--- src/main/java/com/example/demo/Controller/AceController.java
+++ src/main/java/com/example/demo/Controller/AceController.java
@@ -2,9 +2,16 @@ package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 public class AceController {
+	private final HttpSession session;
 	
 	@GetMapping("/")
 	public String GetHome(){
@@ -17,6 +24,15 @@ public class AceController {
 		return "login";
 		
 	}
+	
+	@PostMapping("/Reserve")
+	public String PostReserve(@RequestParam("ename")  String ename){
+		session.setAttribute("ename",ename);
+		
+		return "reserveInput";
+		
+	}
+	
 	
 
 
