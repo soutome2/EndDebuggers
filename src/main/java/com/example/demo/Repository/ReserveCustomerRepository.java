404ce150a28f22a2849b5ab7d93382e2733669c8
Aki330 <ap_aki330@icloud.com>
2024-07-03 10:42:53 +0900

    ユーザーごとの予約確認表示の修正

--- src/main/java/com/example/demo/Repository/ReserveCustomerRepository.java
+++ src/main/java/com/example/demo/Repository/ReserveCustomerRepository.java
@@ -8,12 +8,12 @@ import org.springframework.data.repository.query.Param;
 
 import com.example.demo.Entity.ReserveCustomer;
 
-public interface ReserveCustomerRepository extends JpaRepository<ReserveCustomer, String>{
+public interface ReserveCustomerRepository extends JpaRepository<ReserveCustomer,String>{
 
 	
 	@Query(value="SELECT cid,cname,password,tel,mailaddress,reserveid,ename,"
 			+ "reservedate,reservetime,detail "
-			+ "FROM t_reserve "
-			+ "JOIN m_customer using(cid) WHERE cid=:cid",nativeQuery = true)
-	List<ReserveCustomer>findAIIBycustomerId(@Param("cid") String cid);
+			+ "FROM m_customer "
+			+ "JOIN t_reserve using(cid) WHERE cid=:cid",nativeQuery = true)
+	List<ReserveCustomer> findAIIBycustomerId(@Param("cid") String cid);
 }
