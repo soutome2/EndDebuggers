17e86fc5e8b29fe96e9c885e19f857fd1d77c33d
soutome <soutome.924@gmail.com>
2024-07-10 14:47:06 +0900

    エンティティとか

---
+++ src/main/java/com/example/demo/Entity/Review.java
@@ -0,0 +1,43 @@
+package com.example.demo.Entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "t_review")
+@Data
+
+public class Review {
+	
+	@Id
+	@Column(name = "reviewid")
+	private Integer reviewid;
+	
+	@Column(name = "cid")
+	private String cid;
+	
+	@Column(name = "cname")
+	private String cname;
+	
+	@Column(name = "reviewdate")
+	private LocalDate reviewdate;
+	
+	@Column(name = "reviewtime")
+	private LocalTime reviewtime;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "comment")
+	private String comment;
+	
+	@Column(name = "star")
+	private Integer star;
+
+}
---
+++ src/main/java/com/example/demo/Form/ReviewForm.java
@@ -0,0 +1,14 @@
+package com.example.demo.Form;
+
+import lombok.Data;
+
+@Data
+public class ReviewForm {
+
+	
+	private String cid;
+	private String cname;
+	private String title;
+	private String comment;
+	private Integer star;
+}
