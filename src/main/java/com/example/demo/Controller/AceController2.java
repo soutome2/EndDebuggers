202f8267afb7f6b3bbdae7d1f98a0583f071e277
Aki330 <ap_aki330@icloud.com>
2024-07-16 14:43:42 +0900

    Getでレビューのパラメーター受け取れるようにしました

--- src/main/java/com/example/demo/Controller/AceController2.java
+++ src/main/java/com/example/demo/Controller/AceController2.java
@@ -169,7 +169,8 @@ public class AceController2 {
 	}
 	
 	@GetMapping("/PostReview")
-	public String postReview() {
+	public String postReview(@RequestParam(value = "cid", required = false) String cid,@RequestParam(value = "ename", required = false) String ename,
+			@RequestParam(value = "title", required = false) String title,@RequestParam(value = "comment", required = false) String comment,@RequestParam(value = "star", required = false) Integer star) {
 		
 		// リクエストURLを定義
 		String apiUrl="http://localhost:8080/InsertReview";
@@ -181,11 +182,12 @@ public class AceController2 {
 
 		// 送信するフォームデータを準備
 		ReviewInputForm reviewInputForm = new ReviewInputForm();
-		reviewInputForm.setCid("Gest");
-		reviewInputForm.setEname("佐藤花子");
-		reviewInputForm.setTitle("だめ");
-		reviewInputForm.setComment("ダメだこりゃ");
-		reviewInputForm.setStar(-1);
+		System.out.println(cid);
+		reviewInputForm.setCid(cid);
+		reviewInputForm.setEname(ename);
+		reviewInputForm.setTitle(title);
+		reviewInputForm.setComment(comment);
+		reviewInputForm.setStar(star);
 
 
 		// HTTPリクエストエンティティを作成
