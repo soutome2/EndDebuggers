f997b115f920a5315dbf119c1b790aa78175fee4
skachi0117 <kasyunatts@gmail.com>
2024-07-18 11:29:43 +0900

    テキスト分析APIの追加

---
+++ src/main/java/com/example/demo/Service/TextAnalyticsService.java
@@ -0,0 +1,48 @@
+package com.example.demo.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.azure.ai.textanalytics.TextAnalyticsClient;
+import com.azure.ai.textanalytics.TextAnalyticsClientBuilder;
+import com.azure.ai.textanalytics.models.DocumentSentiment;
+import com.azure.ai.textanalytics.models.SentenceSentiment;
+import com.azure.core.credential.AzureKeyCredential;
+
+@Service
+public class TextAnalyticsService {
+
+	private final String endpoint = "https://acetextanalytics.cognitiveservices.azure.com/";
+	private final String apiKey = "10db3d4fde344e469f01a776d482df8a";
+
+	private final TextAnalyticsClient client;
+
+	public TextAnalyticsService() {
+		this.client = new TextAnalyticsClientBuilder()
+				.credential(new AzureKeyCredential(apiKey))
+				.endpoint(endpoint)
+				.buildClient();
+	}
+
+	public DocumentSentiment analyzeSentiment(String text) {
+		try {
+			DocumentSentiment documentSentiment = client.analyzeSentiment(text, "ja");
+
+			// ログ出力例
+			System.out.println("Document sentiment: " + documentSentiment.getSentiment());
+
+			for (SentenceSentiment sentenceSentiment : documentSentiment.getSentences()) {
+				System.out.println("Sentence: " + sentenceSentiment.getText());
+				System.out.println("Sentiment: " + sentenceSentiment.getSentiment());
+				System.out.println("Positive score: " + sentenceSentiment.getConfidenceScores().getPositive());
+				System.out.println("Negative score: " + sentenceSentiment.getConfidenceScores().getNegative());
+				System.out.println("Neutral score: " + sentenceSentiment.getConfidenceScores().getNeutral());
+			}
+
+			return documentSentiment; // 分析結果を返す
+		} catch (Exception e) {
+			// エラーハンドリング
+			System.err.println("Analyze sentiment error: " + e.getMessage());
+			return null;
+		}
+	}
+}
